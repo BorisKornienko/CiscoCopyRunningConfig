7aae41f3069a7b3b1133cf82a2dba06824b845d9
Корниенко Борис Николаевич <boris.kornienko@metinvestholding.com>
2019-12-22 21:35:21 +0200

    status 30

--- CiscoCopyRunningConfig.go
+++ CiscoCopyRunningConfig.go
@@ -4,21 +4,20 @@
 
 // Пожелания:
 // Должна быть возможность запуска скрипта в планировщике.
-// Список IP коммутаторов и логин пароль могут меняться(один из вариантов скрипт будет брать эту информацию из текстового файла или должна быть возможность менять скрипт) 
+// Список IP коммутаторов и логин пароль могут меняться(один из вариантов скрипт будет брать эту информацию из текстового файла или должна быть возможность менять скрипт)
 
 // encrypt file and decrypt it after With flags -enc
 
+// 10.41.1.3, 10.49.0.11 kbn Qwerty2019 
 
 package main
 
 import (
-	"encoding/xml"
 	"flag"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"log"
 
+	// go install "golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh"
 )
 
@@ -32,12 +31,6 @@ var (
 func main() {
 	flag.Parse()
 
-	// type PrtgErr struct {
-	// 	XMLName   xml.Name `xml:"prtg"`
-	// 	PrtgError int      `xml:"error"`
-	// 	ErrorText string   `xml:"text"`
-	// }
-
 	config := &ssh.ClientConfig{
 		User:            *user,
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
@@ -50,91 +43,24 @@ func main() {
 	client, err := ssh.Dial("tcp", addr, config)
 	if err != nil {
 		// error 1 Device not allow
-		v := &PrtgErr{PrtgError: 1, ErrorText: "Device not available"}
-		outputErr, err := xml.MarshalIndent(v, " ", " ")
-		os.Stdout.Write(outputErr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		}
+		log.Fatal("Device not available: ", err)
 		return
 	}
 
 	session, err := client.NewSession()
 	if err != nil {
 		// panic(err)
-		v := &PrtgErr{PrtgError: 1, ErrorText: "Device not available"}
-		outputErr, err := xml.MarshalIndent(v, " ", " ")
-		os.Stdout.Write(outputErr)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
-		}
+		log.Fatal("Device not available: ", err)
 		return
 	}
 	defer session.Close()
 
-	b, err := session.Output("show mac address-table count")
+	outputString := "copy running-config tftp://10.11.9.2/data/" + *host
+	commandResult, err := session.Output(outputString)
 	if err != nil {
-		v := &PrtgErr{PrtgError: 2, ErrorText: "Unknown output"}
-		outputErr, err := xml.MarshalIndent(v, " ", " ")
-		os.Stdout.Write(outputErr)
-		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		println(outputString)
+		log.Fatal("illegable output: ", err)
 		return
 	}
-
-	// d := strings.Split(string(b), "\n") // "d" It's an array of output lines
-	// // check for right output possibility
-	// // if no ":" in output, so we can't parse with our template
-	// if len(strings.Split(string(b), ":")) < 2 {
-	// 	v := &PrtgErr{PrtgError: 2, ErrorText: "Unknown output"}
-	// 	outputErr, err := xml.MarshalIndent(v, " ", " ")
-	// 	os.Stdout.Write(outputErr)
-	// 	fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	// 	return
-	// }
-
-	// type Result struct {
-	// 	XMLName xml.Name `xml:"result"`
-	// 	Channel string   `xml:"channel"`
-	// 	Value   int      `xml:"value"`
-	// }
-
-	// type Prtg struct {
-	// 	XMLName xml.Name `xml:"prtg"`
-	// 	Results []Result `xml:"result"`
-	// }
-
-	// create a high-level xml teg
-	// v := &Prtg{}
-	// // for each lines we check splitting by ":"
-	// for _, line := range d {
-	// 	// fmt.Println(i)
-	// 	lineParse := strings.Split(line, ":")
-	// 	if len(lineParse) == 2 { // if it contains name of parameter and it's value
-	// 		cha := string(lineParse[0])
-	// 		// fmt.Println(cha)
-	// 		val := strings.TrimSpace(lineParse[1])
-	// 		// fmt.Println(val)
-	// 		if val != "" {
-	// 			val, err := strconv.Atoi(val)
-	// 			// I'm sorry for this
-	// 			if err != nil {
-	// 				// =======================TEMP======================
-	// 				fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	// 				v := &PrtgErr{PrtgError: 22, ErrorText: "Unknown output"}
-	// 				outputErr, err := xml.MarshalIndent(v, " ", " ")
-	// 				os.Stdout.Write(outputErr)
-	// 				if err != nil {
-	// 					fmt.Fprintf(os.Stderr, "error: %v\n", err)
-	// 				}
-	// 				return
-	// 			}
-	// 			v.Results = append(v.Results, Result{Channel: cha, Value: val})
-	// 		}
-	// 	}
-	// }
-	// output, err := xml.MarshalIndent(v, " ", " ")
-	// if err != nil {
-	// 	fmt.Printf("error: %v\n", err)
-	// }
-	// os.Stdout.Write(output)
+	println(commandResult)
 }
